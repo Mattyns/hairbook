86dcc2450a018df942f7db2a8e64fd34ab1abafe
Matheus Everton <81mme.matheus@gmail.com>
2024-08-27 17:45:02 -0300

    atualizei 27/08/2024

--- Script/mensagem2.js
+++ Script/mensagem2.js
@@ -3,9 +3,11 @@ function telademsg() {
     if (window.innerWidth >= 701) {
         var aparecertela = document.getElementById("msgtela").style.display = 'block';
         var botão1 = document.getElementById("msgtelashow").style.display = 'none';
+        var botão1noti = document.getElementById("notitelashow").style.display = 'none';
     } else {
         var aparecertela = document.getElementById("msgtela").style.display = 'none';
         var botão1 = document.getElementById("msgtelashow").style.display = 'none';
+        
 
     }
     
@@ -13,6 +15,30 @@ function telademsg() {
 
 function sumirtelademsg() {
     var sumirtela = document.getElementById("msgtela").style.display ='none';
+    var botão2 = document.getElementById("msgtelashow").style.display = 'block';
+    var botão2noti = document.getElementById("notitelashow").style.display = 'block';
+
+}
+
+
+
+
+function teladenoti() {
+    if (window.innerWidth >= 701) {
+        var aparecertelanoti = document.getElementById("notitela").style.display = 'block';
+        var botão1noti = document.getElementById("notitelashow").style.display = 'none';
+        var botão1 = document.getElementById("msgtelashow").style.display = 'none';
+    } else {
+        var aparecertelanoti = document.getElementById("notitela").style.display = 'none';
+        var botão1noti = document.getElementById("notitelashow").style.display = 'none';
+
+    }
+    
+}
+
+function sumirteladenoti() {
+    var sumirtelanoti = document.getElementById("notitela").style.display ='none';
+    var botão2noti = document.getElementById("notitelashow").style.display = 'block';
     var botão2 = document.getElementById("msgtelashow").style.display = 'block';
 
 }
