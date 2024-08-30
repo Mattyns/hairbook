946b18bae59d5740eb3426477789793b3963f98e
Matheus Everton <81mme.matheus@gmail.com>
2024-08-30 19:48:21 -0300

    Att 30/08/24

---
+++ Script/feed.js
@@ -0,0 +1,109 @@
+function curtir1() {
+    
+    var curtidas = document.getElementById('numcurtidas')
+    var fundolike = document.getElementById('fundolike')
+    var like = window.document.getElementById('like')
+    like.style.color = ('white')
+    fundolike.style.backgroundColor = ('red')
+    curtidas.innerText = '51 Curtidas'
+    like.setAttribute("onclick","descurtir1(this);");
+    
+}
+
+function descurtir1() {
+    var curtidas = document.getElementById('numcurtidas')
+    var fundolike = document.getElementById('fundolike')
+    var like = window.document.getElementById('like')
+    like.style.color = ('black')
+    fundolike.style.backgroundColor = ('rgba(128, 128, 128, 0.259)')
+    curtidas.innerText = '50 Curtidas'
+    like.setAttribute("onclick","curtir1(this);");
+    
+    
+
+
+}
+
+
+
+
+function curtir2() {
+    var curtidas2 = document.getElementById('numcurtidas2')
+    var fundolike2 = document.getElementById('fundolike2')
+    var like2 = window.document.getElementById('like2')
+    like2.style.color = ('white')
+    fundolike2.style.backgroundColor = ('red')
+    curtidas2.innerText = '126 Curtidas'
+    like2.setAttribute("onclick","descurtir2(this);");
+    
+}
+
+function descurtir2() {
+    var curtidas2 = document.getElementById('numcurtidas2')
+    var fundolike2 = document.getElementById('fundolike2')
+    var like2 = window.document.getElementById('like2')
+    like2.style.color = ('black')
+    fundolike2.style.backgroundColor = ('rgba(128, 128, 128, 0.259)')
+    curtidas2.innerText = '125 Curtidas'
+    like2.setAttribute("onclick","curtir2(this);");
+    
+    
+
+
+}
+
+
+
+
+function curtir3() {
+    var curtidas3 = document.getElementById('numcurtidas3')
+    var fundolike3 = document.getElementById('fundolike3')
+    var like3 = window.document.getElementById('like3')
+    like3.style.color = ('white')
+    fundolike3.style.backgroundColor = ('red')
+    curtidas3.innerText = '46 Curtidas'
+    like3.setAttribute("onclick","descurtir3(this);");
+    
+}
+
+function descurtir3() {
+    var curtidas3 = document.getElementById('numcurtidas3')
+    var fundolike3 = document.getElementById('fundolike3')
+    var like3 = window.document.getElementById('like3')
+    like3.style.color = ('black')
+    fundolike3.style.backgroundColor = ('rgba(128, 128, 128, 0.259)')
+    curtidas3.innerText = '45 Curtidas'
+    like3.setAttribute("onclick","curtir3(this);");
+    
+    
+
+
+}
+
+
+
+
+function curtir4() {
+    var curtidas = document.getElementById('numcurtidas')
+    var fundolike = document.getElementById('fundolike')
+    var like = window.document.getElementById('like')
+    like.style.color = ('white')
+    fundolike.style.backgroundColor = ('red')
+    curtidas.innerText = '51 Curtidas'
+    like.setAttribute("onclick","descurtir4(this);");
+    
+}
+
+function descurtir4() {
+    var curtidas = document.getElementById('numcurtidas')
+    var fundolike = document.getElementById('fundolike')
+    var like = window.document.getElementById('like')
+    like.style.color = ('black')
+    fundolike.style.backgroundColor = ('rgba(128, 128, 128, 0.259)')
+    curtidas.innerText = '50 Curtidas'
+    like.setAttribute("onclick","curtir4(this);");
+    
+    
+
+
+}
